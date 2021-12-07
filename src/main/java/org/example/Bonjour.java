66adda367435946a1d6d1267e9f560063f2a7a60
Korentin K <korentin.kolaczek@epsi.fr>
2021-12-07 09:44:20 +0100

    test si saluer Cloud renvoie le bon résultat

--- src/main/java/org/example/Bonjour.java
+++ src/main/java/org/example/Bonjour.java
@@ -2,4 +2,20 @@ package org.example;
 
 public class Bonjour {
 
+    public String saluer(String nom)
+    {
+        int compteurMaj=0;
+        for (int i=0;i<nom.length();i++)
+        {
+            if(Character.isUpperCase(nom.indexOf(i)))
+            {
+                compteurMaj=+1;
+            }
+        }
+        if (compteurMaj <=1)
+        {
+            return "Bonjour, "+nom+".";
+        }
+        return "Bojour, l'ami.";
+    }
 }
